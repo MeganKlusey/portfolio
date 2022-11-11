d56825d9f9f4fc8deedf0d8b95ae858a7b4f36ff
Megan Klusey <megan@toru.digital>
2022-11-11 23:21:35 +0000

    Silver styling and animation

--- src/client/js/views/home.js
+++ src/client/js/views/home.js
@@ -12,14 +12,14 @@ class Home extends React.Component {
               <polygon class='arrow-bottom' points='37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1'/>
             </svg>
             <svg class='gradient more-arrows'>
-            <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-              <stop offset="0" stop-color="rgba(187,187,187,0)" />
-              <stop offset="0.94" stop-color="rgba(255,255,255,0.75)" />
-              <stop offset="0.95" stop-color="rgba(255,255,255,0.75)" />
-            </linearGradient>
-              <polygon fill="url(#gradient)" class='arrow-top' points='37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3'/>
-              <polygon fill="url(#gradient)" class='arrow-middle' points='37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7'/>
-              <polygon fill="url(#gradient)" class='arrow-bottom' points='37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1'/>
+              <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+                <stop offset="0" stop-color="rgba(187,187,187,0)"/>
+                <stop offset="0.94" stop-color="rgba(255,255,255,0.75)"/>
+                <stop offset="0.95" stop-color="rgba(255,255,255,0.75)"/>
+              </linearGradient>
+              <polygon class='arrow-top' fill="url(#gradient)" points='37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3'/>
+              <polygon class='arrow-middle' fill="url(#gradient)" points='37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7'/>
+              <polygon class='arrow-bottom' fill="url(#gradient)" points='37.6,64 0,36.1 5.1,32.8 37.6,56.8 70.4,32.8 75.5,36.1'/>
             </svg>
           </div>
         </a>
