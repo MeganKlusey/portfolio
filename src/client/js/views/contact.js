cac1c61e8d21e2ed00dda5828abc92db16b941fa
Megan Klusey <megan@toru.digital>
2023-03-15 13:55:13 +0000

    Format

--- src/client/js/views/contact.js
+++ src/client/js/views/contact.js
@@ -4,16 +4,16 @@ class Contact extends React.Component {
   render() {
     return (
       <>
-        <form id='contactForm' onSubmit='submitForm();'>
-          <p id='contactDescription'>If you have an enquiry or looking to hire, please get in touch using the form below.</p>
+        <form id='contact-form' onSubmit='submitForm();'>
+          <p id='contact-description'>If you have an enquiry or looking to hire, please get in touch using the form below.</p>
           <br/>
-          <input id='name' name='name' type='text' class='formControl' placeholder='Name..' required />
+          <input id='name' name='name' type='text' class='form-control' placeholder='Name..' required />
           <br/>
-          <input id='email' name='email' type='email' class='formControl' placeholder='Email address..' required />
+          <input id='email' name='email' type='email' class='form-control' placeholder='Email address..' required />
           <br/>
-          <textarea id='message' name='message' class='formControl' placeholder='Your message..' required ></textarea>
+          <textarea id='message' name='message' class='form-control' placeholder='Your message..' required ></textarea>
           <br/>
-          <input id='submit' type='submit' class='formControl submit' value='SEND' />
+          <input id='submit' type='submit' class='form-control submit' value='SEND' />
           <br/>
           <span id='status'></span>
         </form>
