8c73453b323e2ac968f3dd01d0d00b9ee76d0d99
Megan Klusey <megan@toru.digital>
2023-03-27 22:13:25 +0100

    Render root

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,10 @@
-import './styles/styles.scss'
-import './js/birds'
-import './js/views'
-import './js/script'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
