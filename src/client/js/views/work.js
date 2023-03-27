d06abc637589c6b849cbfbced1fdf3017c123ed4
Megan Klusey <megan@toru.digital>
2023-03-28 00:34:42 +0100

    Remove image sources and onClick functions

--- src/client/js/views/work.js
+++ src/client/js/views/work.js
@@ -5,91 +5,91 @@ import React from 'react'
 const Work = (props) => {
   return (
     <div className='work-container'>
-      <div className='thumbnail-div' onClick="on()" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
+      <div className='thumbnail-div' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
         <div className='box'>
           <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
             <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
             <line className='left' x1='0' y1='223.69' x2='0' y2='-447.38'/>
             <line className='bottom' x1='460' y1='223.69' x2='-920' y2='223.69'/>
             <line className='right' x1='460' y1='-447.38' x2='460' y2='671.07'/>
-            <img src='berlineastsidegalleryberlin.png' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
+            <img src='' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
           </svg>
         </div>
       </div>
-      <div className='thumbnail-div' onClick="on()" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
+      <div className='thumbnail-div' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
         <div className='box'>
           <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
             <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
             <line className='left' x1='0' y1='223.69' x2='0' y2='-447.38'/>
             <line className='bottom' x1='460' y1='223.69' x2='-920' y2='223.69'/>
             <line className='right' x1='460' y1='-447.38' x2='460' y2='671.07'/>
-            <img src='berlineastsidegalleryberlin.png' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
+            <img src='' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
           </svg>
         </div>
       </div>
-      <div className='thumbnail-div' onClick="on()" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
+      <div className='thumbnail-div' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
         <div className='box'>
           <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
             <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
             <line className='left' x1='0' y1='223.69' x2='0' y2='-447.38'/>
             <line className='bottom' x1='460' y1='223.69' x2='-920' y2='223.69'/>
             <line className='right' x1='460' y1='-447.38' x2='460' y2='671.07'/>
-            <img src='berlineastsidegalleryberlin.png' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
+            <img src='' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
           </svg>
         </div>
       </div>
-      <div className='thumbnail-div' onClick="on()" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
+      <div className='thumbnail-div' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
         <div className='box'>
           <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
             <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
             <line className='left' x1='0' y1='223.69' x2='0' y2='-447.38'/>
             <line className='bottom' x1='460' y1='223.69' x2='-920' y2='223.69'/>
             <line className='right' x1='460' y1='-447.38' x2='460' y2='671.07'/>
-            <img src='berlineastsidegalleryberlin.png' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
+            <img src='' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
           </svg>
         </div>
       </div>
-      <div className='thumbnail-div' onClick="on()" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
+      <div className='thumbnail-div' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
         <div className='box'>
           <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
             <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
             <line className='left' x1='0' y1='223.69' x2='0' y2='-447.38'/>
             <line className='bottom' x1='460' y1='223.69' x2='-920' y2='223.69'/>
             <line className='right' x1='460' y1='-447.38' x2='460' y2='671.07'/>
-            <img src='berlineastsidegalleryberlin.png' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
+            <img src='' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
           </svg>
         </div>
       </div>
-      <div className='thumbnail-div' onClick="on()" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
+      <div className='thumbnail-div' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
         <div className='box'>
           <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
             <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
             <line className='left' x1='0' y1='223.69' x2='0' y2='-447.38'/>
             <line className='bottom' x1='460' y1='223.69' x2='-920' y2='223.69'/>
             <line className='right' x1='460' y1='-447.38' x2='460' y2='671.07'/>
-            <img src='berlineastsidegalleryberlin.png' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
+            <img src='' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
           </svg>
         </div>
       </div>
-      <div className='thumbnail-div' onClick="on()" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
+      <div className='thumbnail-div' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
         <div className='box'>
           <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
             <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
             <line className='left' x1='0' y1='223.69' x2='0' y2='-447.38'/>
             <line className='bottom' x1='460' y1='223.69' x2='-920' y2='223.69'/>
             <line className='right' x1='460' y1='-447.38' x2='460' y2='671.07'/>
-            <img src='berlineastsidegalleryberlin.png' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
+            <img src='' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
           </svg>
         </div>
       </div>
-      <div className='thumbnail-div' onClick="on()" data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
+      <div className='thumbnail-div' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
         <div className='box'>
           <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
             <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
             <line className='left' x1='0' y1='223.69' x2='0' y2='-447.38'/>
             <line className='bottom' x1='460' y1='223.69' x2='-920' y2='223.69'/>
             <line className='right' x1='460' y1='-447.38' x2='460' y2='671.07'/>
-            <img src='berlineastsidegalleryberlin.png' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
+            <img src='' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
           </svg>
         </div>
       </div>
