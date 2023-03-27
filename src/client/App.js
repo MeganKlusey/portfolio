02f752258039d05d4c7243798a02da7cd1c26ecc
Megan Klusey <megan@toru.digital>
2023-03-27 22:09:15 +0100

    App set up

---
+++ src/client/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Header from '../client/js/views/header'
+import Home from '../client/js/views/home'
+import About from '../client/js/views/about'
+import Work from '../client/js/views/work'
+import Contact from '../client/js/views/contact'
+
+import '../client/styles/styles.scss'
+
+function App() {
+  return (
+		<div className="App">
+      <Header />
+      <Home />
+      <About />
+      <Work />
+      <Contact />
+		</div>
+  )
+}
+
+export default App;
