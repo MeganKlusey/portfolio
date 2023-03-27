2f3282484f0b56bdbf09ccc11a24c0bd91e6201a
Megan Klusey <megan@toru.digital>
2023-03-28 00:41:53 +0100

    Add another container to header component

--- src/client/js/views/header.js
+++ src/client/js/views/header.js
@@ -4,26 +4,28 @@ import React from 'react'
 
 const Header = (props) => {
   return (
-    <div className="header">
-      <div className='title-wrapper'>
-        <h1 className='base'><a className='title-btn scroll' href='#home'><div className="shine">MEGAN KLUSEY</div></a></h1>
-        <h1 className='gradient' data-text='MEGAN KLUSEY'><a className='title-btn scroll' href='#home'></a></h1>
-      </div>
-      <nav>
-        <div className='about-wrapper'>
-          <li className='base'><a className='nav-btn scroll' href='#about'><div className="shine about">ABOUT</div></a></li>
-          <li className='gradient' data-text='ABOUT'><a className='nav-btn scroll' href='#about'></a></li>
+    <div className="header-container">
+      <div className="header">
+        <div className='title-wrapper'>
+          <h1 className='base'><a className='title-btn scroll' href='#home'><div className="shine">MEGAN KLUSEY</div></a></h1>
+          <h1 className='gradient' data-text='MEGAN KLUSEY'><a className='title-btn scroll' href='#home'></a></h1>
         </div>
-        <div className='work-wrapper'>
-          <li className='base'><a className='nav-btn scroll' href='#work'><div className="shine work">WORK</div></a></li>
-          <li className='gradient' data-text='WORK'><a className='nav-btn scroll' href='#work'></a></li>
-        </div>
-        <div className='contact-wrapper'>
-          <li className='base'><a className='nav-btn scroll' href='#contact'><div className="shine contact">CONTACT</div></a></li>
-          <li className='gradient' data-text='CONTACT'><a className='nav-btn scroll' href='#contact'></a></li>
-        </div>
-      </nav>
-    </div>  
+        <nav>
+          <div className='about-wrapper'>
+            <li className='base'><a className='nav-btn scroll' href='#about'><div className="shine about">ABOUT</div></a></li>
+            <li className='gradient' data-text='ABOUT'><a className='nav-btn scroll' href='#about'></a></li>
+          </div>
+          <div className='work-wrapper'>
+            <li className='base'><a className='nav-btn scroll' href='#work'><div className="shine work">WORK</div></a></li>
+            <li className='gradient' data-text='WORK'><a className='nav-btn scroll' href='#work'></a></li>
+          </div>
+          <div className='contact-wrapper'>
+            <li className='base'><a className='nav-btn scroll' href='#contact'><div className="shine contact">CONTACT</div></a></li>
+            <li className='gradient' data-text='CONTACT'><a className='nav-btn scroll' href='#contact'></a></li>
+          </div>
+        </nav>
+      </div> 
+    </div> 
   );
 }
 
