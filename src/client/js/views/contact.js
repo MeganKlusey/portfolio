ae551f6181277e870e417f5b2ecde3f18f386057
Megan Klusey <megan@toru.digital>
2023-09-05 19:44:12 +0100

    Add shine to form button on hover

--- src/client/js/views/contact.js
+++ src/client/js/views/contact.js
@@ -6,16 +6,16 @@ const Contact = (props) => {
   return (
     <div className="contact-container">
       <div className='contact-form-wrapper'>
-        <p className='contact-description'>Please get in touch using the form below.</p>
+        <p className='contact-description'>Feel free to get in touch!</p>
         <br/>
         <form className='contact-form' onSubmit={props.submitForm}>
-          <input id='name' name='name' type='text' className='form-control' placeholder='Name..' required />
+          <input id='name' name='name' type='text' className='form-control' placeholder='name' required />
           <br/>
-          <input id='email' name='email' type='email' className='form-control' placeholder='Email address..' required />
+          <input id='email' name='email' type='email' className='form-control' placeholder='email address' required />
           <br/>
-          <textarea id='message' name='message' className='form-control' placeholder='Your message..' required ></textarea>
+          <textarea id='message' name='message' className='form-control' placeholder='your message' required ></textarea>
           <br/>
-          <input id='submit' type='submit' className='form-control submit' value='SEND' />
+          <input id='submit' type='submit' className='form-control submit shine' value='SEND' />
           <br/>
           <span id='status'></span>
         </form>
