41311931d22cb4d51a4019b32e0927ba259a05e5
Megan Klusey <megan@toru.digital>
2024-02-06 20:24:16 +0000

    Add aos to contact section and placeholder amends

--- src/client/js/views/contact.js
+++ src/client/js/views/contact.js
@@ -4,23 +4,26 @@ import React from 'react'
 
 const Contact = (props) => {
   return (
-    <div className="contact-container" id="contact">
+    <section className="contact-container" id="contact">
       <div className='contact-form-wrapper'>
-        <p className='contact-description'>Feel free to get in touch!</p>
-        <br/>
+        <p className='contact-description' data-aos="zoom-in">Please get in touch by e&#8209;mailing &nbsp; 
+         <a href="mailto:megan.klusey@hotmail.co.uk">megan.klusey@hotmail.co.uk</a> <br/> or by 
+         using the form below:</p>
         <form className='contact-form' onSubmit={props.submitForm}>
-          <input id='name' name='name' type='text' className='form-control' placeholder='name' required />
+          <input id='name' name='name' type='text' className='form-control' placeholder='your name' required data-aos="fade-right" />
           <br/>
-          <input id='email' name='email' type='email' className='form-control' placeholder='email address' required />
+          <input id='email' name='email' type='email' className='form-control' placeholder='your email address' required data-aos="fade-right" />
           <br/>
-          <textarea id='message' name='message' className='form-control' placeholder='your message' required ></textarea>
+          <input id='subject' name='name' type='text' className='form-control' placeholder='subject' required data-aos="fade-right" />
           <br/>
-          <input id='submit' type='submit' className='form-control submit shine' value='SEND' />
+          <textarea id='message' name='message' className='form-control' placeholder='your message' required data-aos="fade-right"></textarea>
+          <br/>
+          <input id='submit' type='submit' className='form-control submit shine' value='SEND' data-aos="zoom-out" />
           <br/>
           <span id='status'></span>
         </form>
       </div>
-    </div> 
+    </section> 
   )
 }
 
