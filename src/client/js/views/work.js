f4d8dbefeeb333a9d07e61e999004867ee76db11
Megan Klusey <megan@toru.digital>
2024-02-06 20:38:28 +0000

    Work aos and remove containers

--- src/client/js/views/work.js
+++ src/client/js/views/work.js
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Work = (props) => {
   return (
-    <div className='work-container' id="work">
+    <section className='work-container' id="work">
       <div className='thumbnails-container'>
         <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
           <div className='box'>
@@ -17,7 +17,7 @@ const Work = (props) => {
             </svg>
           </div>
         </div>
-        <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
+        <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" data-aos-delay="100">
           <div className='box'>
             <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
               <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
@@ -28,7 +28,7 @@ const Work = (props) => {
             </svg>
           </div>
         </div>
-        <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
+        <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" data-aos-delay="200">
           <div className='box'>
             <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
               <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
@@ -39,51 +39,7 @@ const Work = (props) => {
             </svg>
           </div>
         </div>
-        <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
-          <div className='box'>
-            <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
-              <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
-              <line className='left' x1='0' y1='223.69' x2='0' y2='-447.38'/>
-              <line className='bottom' x1='460' y1='223.69' x2='-920' y2='223.69'/>
-              <line className='right' x1='460' y1='-447.38' x2='460' y2='671.07'/>
-              <img src='' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
-            </svg>
-          </div>
-        </div>
-        <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
-          <div className='box'>
-            <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
-              <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
-              <line className='left' x1='0' y1='223.69' x2='0' y2='-447.38'/>
-              <line className='bottom' x1='460' y1='223.69' x2='-920' y2='223.69'/>
-              <line className='right' x1='460' y1='-447.38' x2='460' y2='671.07'/>
-              <img src='' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
-            </svg>
-          </div>
-        </div>
-        <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
-          <div className='box'>
-            <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
-              <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
-              <line className='left' x1='0' y1='223.69' x2='0' y2='-447.38'/>
-              <line className='bottom' x1='460' y1='223.69' x2='-920' y2='223.69'/>
-              <line className='right' x1='460' y1='-447.38' x2='460' y2='671.07'/>
-              <img src='' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
-            </svg>
-          </div>
-        </div>
-        <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
-          <div className='box'>
-            <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
-              <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
-              <line className='left' x1='0' y1='223.69' x2='0' y2='-447.38'/>
-              <line className='bottom' x1='460' y1='223.69' x2='-920' y2='223.69'/>
-              <line className='right' x1='460' y1='-447.38' x2='460' y2='671.07'/>
-              <img src='' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
-            </svg>
-          </div>
-        </div>
-        <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom">
+        <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="bottom-bottom" data-aos-delay="300">
           <div className='box'>
             <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
               <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
@@ -95,7 +51,7 @@ const Work = (props) => {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
