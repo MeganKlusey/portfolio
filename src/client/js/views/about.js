087b6b16df7fd66051416040774b8c038e40e9b1
Megan Klusey <megan@toru.digital>
2024-02-06 20:24:04 +0000

    Add aos to about section

--- src/client/js/views/about.js
+++ src/client/js/views/about.js
@@ -4,9 +4,9 @@ import React from 'react'
 
 const About = (props) => {
   return (
-    <div className='about-container' id="about">
+    <section className='about-container' id="about" data-aos-anchor-placement="top-center" data-aos="zoom-in" data-aos-duration="500">
       <p className='aboutText'>
-        I'm a web developer, and passionate about it as it provides the opportunity to combine the 
+        I'm a front end developer, and passionate about it as it provides the opportunity to combine the 
         analytic with the creative. With a degree in Business Management and Computer Science from Oxford Brookes University,
         I am now looking for a role in front-end development to take my skills to the next level. My knowledge of languages include:
         <br/><br/>
@@ -16,10 +16,8 @@ const About = (props) => {
         and proficient in Microsoft Office software. Digital marketing
         also piques my interest, with a qualification in Google Analytics and 
         understanding of building SEO-friendly websites.
-        <br/><br/>
-        Please check out my work below and feel free to get in contact.
       </p>
-    </div>
+    </section>
   )
 }
 
