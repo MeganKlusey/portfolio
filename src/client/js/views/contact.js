107c6a5452bb1f894b5bdfb19815e5fe4102b071
Megan Klusey <megan@toru.digital>
2024-02-23 21:51:54 +0000

    Add subject input to form and aos

--- src/client/js/views/contact.js
+++ src/client/js/views/contact.js
@@ -6,7 +6,7 @@ const Contact = (props) => {
   return (
     <section className="contact-container" id="contact">
       <div className='contact-form-wrapper'>
-        <p className='contact-description' data-aos="zoom-in">Please get in touch by e&#8209;mailing &nbsp; 
+        <p className='contact-description' data-aos="zoom-in">Please get in touch by e&#8209;mailing<br/> 
          <a href="mailto:megan.klusey@hotmail.co.uk">megan.klusey@hotmail.co.uk</a> <br/> or by 
          using the form below:</p>
         <form className='contact-form' onSubmit={props.submitForm}>
@@ -14,11 +14,11 @@ const Contact = (props) => {
           <br/>
           <input id='email' name='email' type='email' className='form-control' placeholder='your email address' required data-aos="fade-right" />
           <br/>
-          <input id='subject' name='name' type='text' className='form-control' placeholder='subject' required data-aos="fade-right" />
+          <input id='subject' name='subject' type='text' className='form-control' placeholder='subject' required data-aos="fade-right" />
           <br/>
           <textarea id='message' name='message' className='form-control' placeholder='your message' required data-aos="fade-right"></textarea>
           <br/>
-          <input id='submit' type='submit' className='form-control submit shine' value='SEND' data-aos="zoom-out" />
+          <input id='submit' type='submit' className='form-control submit shine' value='SEND' data-aos="zoom-out" data-aos-anchor-placement="center-bottom" />
           <br/>
           <span id='status'></span>
         </form>
