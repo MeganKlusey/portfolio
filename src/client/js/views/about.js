6b91d7e30ca76b61b4631a103ffe857eca8b3bb2
Megan Klusey <megan@toru.digital>
2024-02-23 22:08:36 +0000

    Add inner wrapper to content

--- src/client/js/views/about.js
+++ src/client/js/views/about.js
@@ -5,18 +5,20 @@ import React from 'react'
 const About = (props) => {
   return (
     <section className='about-container' id="about">
-      <p className='aboutText' data-aos-anchor-placement="top-center" data-aos="zoom-in" data-aos-duration="300">
-        I'm a front end developer, and passionate about it as it provides the opportunity to combine the 
-        analytic with the creative. With a degree in Business Management and Computer Science from Oxford Brookes University,
-        I am now looking for a role in front-end development to take my skills to the next level. My knowledge of languages include:
-        <br/><br/>
-        HTML, CSS, JavaScript, jQuery, React, PHP, Java, Python, XML and SVG.
-        <br/><br/>
-        In addition to this, I have experience using WordPress
-        and proficient in Microsoft Office software. Digital marketing
-        also piques my interest, with a qualification in Google Analytics and 
-        understanding of building SEO-friendly websites.
-      </p>
+      <div>
+        <p className='aboutText' data-aos-anchor-placement="top-center" data-aos="zoom-in" data-aos-duration="300">
+          I'm a front end developer, and passionate about it as it provides the opportunity to combine the 
+          analytic with the creative. With a degree in Business Management and Computer Science from Oxford Brookes University,
+          I am now looking for a role in front-end development to take my skills to the next level. My knowledge of languages include:
+          <br/><br/>
+          HTML, CSS, JavaScript, jQuery, React, PHP, Java, Python, XML and SVG.
+          <br/><br/>
+          In addition to this, I have experience using WordPress
+          and proficient in Microsoft Office software. Digital marketing
+          also piques my interest, with a qualification in Google Analytics and 
+          understanding of building SEO-friendly websites.
+        </p>
+      </div>
     </section>
   )
 }
