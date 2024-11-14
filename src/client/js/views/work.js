6778e931ad013bd5ab9fb1707e24a8e79401a20f
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-11-14 22:52:09 +0000

    Rework thumbnails

--- src/client/js/views/work.js
+++ src/client/js/views/work.js
@@ -5,40 +5,20 @@ import React from 'react'
 const Work = (props) => {
   return (
     <section className='work-container' id="work">
-      <div>
-        <div className='thumbnails-container'>
-          <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="center-bottom">
-            <div className='box'>
-              <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
-                <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
-                <line className='left' x1='0' y1='223.69' x2='0' y2='-447.38'/>
-                <line className='bottom' x1='460' y1='223.69' x2='-920' y2='223.69'/>
-                <line className='right' x1='460' y1='-447.38' x2='460' y2='671.07'/>
-                <img src='' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
-              </svg>
-            </div>
+      <div className='thumbnails-container'>
+        <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="center-bottom">
+          <div class="thumbnail-wrapper">
+            <img src='' className='thumbnail' alt=''/>
           </div>
-          <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="center-bottom">
-            <div className='box'>
-              <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
-                <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
-                <line className='left' x1='0' y1='223.69' x2='0' y2='-447.38'/>
-                <line className='bottom' x1='460' y1='223.69' x2='-920' y2='223.69'/>
-                <line className='right' x1='460' y1='-447.38' x2='460' y2='671.07'/>
-                <img src='' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
-              </svg>
-            </div>
+        </div>
+        <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-delay="50">
+          <div class="thumbnail-wrapper">
+            <img src='' className='thumbnail' alt=''/>
           </div>
-          <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-delay="100">
-            <div className='box'>
-              <svg xmlns='http://www.w3.org/2000/svg' width='100%' height='100%'>
-                <line className='top' x1='-920' y1='0' x2='460' y2='0'/>
-                <line className='left' x1='0' y1='223.69' x2='0' y2='-447.38'/>
-                <line className='bottom' x1='460' y1='223.69' x2='-920' y2='223.69'/>
-                <line className='right' x1='460' y1='-447.38' x2='460' y2='671.07'/>
-                <img src='' className='thumbnail' alt='Berlin East Side Gallery Berlin Thumbnail'/>
-              </svg>
-            </div>
+        </div>
+        <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-delay="100">
+          <div class="thumbnail-wrapper">
+            <img src='' className='thumbnail' alt=''/>
           </div>
         </div>
       </div>
