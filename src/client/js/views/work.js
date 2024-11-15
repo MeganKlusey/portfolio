a411dbf94ffe47b502b1d605f30314a81f5b59ea
MeganKlusey <megan.klusey@hotmail.co.uk>
2024-11-15 21:30:51 +0000

    Add thumbnail overlay

--- src/client/js/views/work.js
+++ src/client/js/views/work.js
@@ -8,16 +8,25 @@ const Work = (props) => {
       <div className='thumbnails-container'>
         <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="center-bottom">
           <div class="thumbnail-wrapper">
+            <div className='thumbnail-overlay slide-up'>
+              <a href="">View project</a>
+            </div>
             <img src='' className='thumbnail' alt=''/>
           </div>
         </div>
         <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-delay="50">
           <div class="thumbnail-wrapper">
+            <div className='thumbnail-overlay slide-up'>
+              <a href="">View project</a>
+            </div>
             <img src='' className='thumbnail' alt=''/>
           </div>
         </div>
         <div className='thumbnail-container' data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-delay="100">
           <div class="thumbnail-wrapper">
+            <div className='thumbnail-overlay slide-up'>
+              <a href="">View project</a>
+            </div>
             <img src='' className='thumbnail' alt=''/>
           </div>
         </div>
