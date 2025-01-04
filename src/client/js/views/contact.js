48c995e33280b7c4ca0ee3be30acff4e8c6bea1e
MeganKlusey <megan.klusey@hotmail.co.uk>
2025-01-04 16:19:30 +0000

    Add contact form back in

--- src/client/js/views/contact.js
+++ src/client/js/views/contact.js
@@ -8,6 +8,18 @@ const Contact = (props) => {
       <div className='contact-form-wrapper'>
         <p className='contact-description' data-aos="zoom-in">Please get in touch by e&#8209;mailing me at<br/> 
          <a href="mailto:megan.klusey@hotmail.co.uk">megan.klusey@hotmail.co.uk</a></p>
+         <br/>
+         <form className='contact-form' onSubmit={props.submitForm}>
+          <input id='name' name='name' type='text' className='form-control' placeholder='your full name' required data-aos="fade-right" />
+          <br/>
+          <input id='email' name='email' type='email' className='form-control' placeholder='your email address' required data-aos="fade-right" />
+          <br/>
+          <textarea id='message' name='message' className='form-control' placeholder='your message' required data-aos="fade-right"></textarea>
+          <br/>
+          <input id='submit' type='submit' className='form-control submit' value='SEND' data-aos="zoom-out" data-aos-anchor-placement="center-bottom" />
+          <br/>
+          <span id='status'></span>
+        </form>
       </div>
     </section> 
   )
