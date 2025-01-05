998fa6f1ef4d8c8f9609ad42cf24d9eb4cac6f63
MeganKlusey <megan.klusey@hotmail.co.uk>
2025-01-05 16:24:54 +0000

    Amend placeholder

--- src/client/js/views/contact.js
+++ src/client/js/views/contact.js
@@ -12,7 +12,7 @@ const Contact = (props) => {
          <form className='contact-form' onSubmit={props.submitForm}>
           <input id='name' name='name' type='text' className='form-control' placeholder='your name' required data-aos="fade-right" />
           <br/>
-          <input id='email' name='email' type='email' className='form-control' placeholder='your email address' required data-aos="fade-right" />
+          <input id='email' name='email' type='email' className='form-control' placeholder='your e-mail address' required data-aos="fade-right" />
           <br/>
           <textarea id='message' name='message' className='form-control' placeholder='your message' required data-aos="fade-right"></textarea>
           <br/>
