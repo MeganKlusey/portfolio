2001165e0c5103002a88b4ab904206ec0b45c349
MeganKlusey <megan.klusey@hotmail.co.uk>
2025-01-05 16:20:14 +0000

    Update contact description and width

--- src/client/js/views/contact.js
+++ src/client/js/views/contact.js
@@ -6,7 +6,7 @@ const Contact = (props) => {
   return (
     <section className="contact-container" id="contact">
       <div className='contact-form-wrapper'>
-        <p className='contact-description' data-aos="zoom-in">Please get in touch by e&#8209;mailing me at<br/> 
+        <p className='contact-description' data-aos="zoom-in">Please get in touch by using the form below or by e&#8209;mailing me at<br/> 
          <a href="mailto:megan.klusey@hotmail.co.uk">megan.klusey@hotmail.co.uk</a></p>
          <br/>
          <form className='contact-form' onSubmit={props.submitForm}>
