fcd41025778c695e679438f0b14608f101362c8f
MeganKlusey <megan.klusey@hotmail.co.uk>
2025-01-05 19:29:22 +0000

    Hook up formspree

--- src/client/js/views/contact.js
+++ src/client/js/views/contact.js
@@ -9,7 +9,7 @@ const Contact = (props) => {
         <p className='contact-description' data-aos="zoom-in">Please get in touch by using the form below or by e&#8209;mailing me at<br/> 
          <a href="mailto:megan.klusey@hotmail.co.uk">megan.klusey@hotmail.co.uk</a></p>
          <br/>
-         <form className='contact-form' onSubmit={props.submitForm}>
+         <form className='contact-form' action="https://formspree.io/f/myzzbdlb" method="POST">
           <input id='name' name='name' type='text' className='form-control' placeholder='your name' required data-aos="fade-right" />
           <br/>
           <input id='email' name='email' type='email' className='form-control' placeholder='your e-mail address' required data-aos="fade-right" />
@@ -18,7 +18,6 @@ const Contact = (props) => {
           <br/>
           <input id='submit' type='submit' className='form-control submit' value='SEND' data-aos="zoom-out" data-aos-anchor-placement="center-bottom" />
           <br/>
-          <span id='status'></span>
         </form>
       </div>
     </section> 
