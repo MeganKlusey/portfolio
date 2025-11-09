9b76b64b7a692e5cfc872a92b1b3c62c7294901a
MeganKlusey <megan.klusey@hotmail.co.uk>
2025-11-09 22:47:12 +0000

    Position work content progress

--- src/client/js/views/work.js
+++ src/client/js/views/work.js
@@ -12,264 +12,270 @@ import DominoJewellery from "../img/domino-jewellery.png";
 const Work = (props) => {
   return (
     <section className="work-container" id="work">
-      <div className="work-category">
-        <h3>Personal Projects:</h3>
-        <div className="thumbnails-container">
-          <div
-            className="thumbnail-container"
-            data-aos="fade-up"
-            data-aos-anchor-placement="center-bottom"
-          >
-            <div className="thumbnail-wrapper">
-              <div className="thumbnail-overlay">
-                <div className="overlay-content">
-                  <div className="project-info">
-                    <h4>myMedium</h4>
-                    <p>
-                      <span className="list-title">Tech Stack:</span>&nbsp;
-                      <span className="list">
-                        React, Node.js, Tailwind, jQuery, REST API, AWS
-                      </span>
-                    </p>
-                  </div>
-                  <a
-                    href="https://meganklusey.github.io/mymedium/"
-                    className="view-project"
-                    target="_blank"
-                  >
-                    <p>View project</p>
-                    <div className="chevron-forward">
-                      <ion-icon name="chevron-forward-outline"></ion-icon>
+      <div className="work-categories">
+        <div className="work-category">
+          <h3>Personal Projects:</h3>
+          <div className="thumbnails-container">
+            <div
+              className="thumbnail-container"
+              data-aos="fade-up"
+              data-aos-anchor-placement="center-bottom"
+            >
+              <div className="thumbnail-wrapper">
+                <div className="thumbnail-overlay">
+                  <div className="overlay-content">
+                    <div className="project-info">
+                      <h4>myMedium</h4>
+                      <p>
+                        <span className="list-title">Tech Stack:</span>&nbsp;
+                        <span className="list">
+                          React, Node.js, Tailwind, jQuery, REST API, AWS
+                        </span>
+                      </p>
                     </div>
-                  </a>
+                    <a
+                      href="https://meganklusey.github.io/mymedium/"
+                      className="view-project"
+                      target="_blank"
+                    >
+                      <p>View project</p>
+                      <div className="chevron-forward">
+                        <ion-icon name="chevron-forward-outline"></ion-icon>
+                      </div>
+                    </a>
+                  </div>
                 </div>
+                <img
+                  src={MyMedium}
+                  className="thumbnail"
+                  alt="My Medium thumbnail"
+                />
               </div>
-              <img
-                src={MyMedium}
-                className="thumbnail"
-                alt="My Medium thumbnail"
-              />
             </div>
-          </div>
-          <div
-            className="thumbnail-container"
-            data-aos="fade-up"
-            data-aos-anchor-placement="center-bottom"
-            data-aos-delay="50"
-          >
-            <div className="thumbnail-wrapper">
-              <div className="thumbnail-overlay">
-                <div className="overlay-content">
-                  <div className="project-info">
-                    <h4>The 10 Vinyls</h4>
-                    <p>
-                      <span className="list-title">Tech Stack:</span>&nbsp;
-                      <span className="list">Vue.js, SCSS, TypeScript</span>
-                    </p>
-                  </div>
-                  <a
-                    href="https://meganklusey.github.io/the-10-vinyls/"
-                    className="view-project"
-                    target="_blank"
-                  >
-                    <p>View project</p>
-                    <div className="chevron-forward">
-                      <ion-icon name="chevron-forward-outline"></ion-icon>
+            <div
+              className="thumbnail-container"
+              data-aos="fade-up"
+              data-aos-anchor-placement="center-bottom"
+              data-aos-delay="50"
+            >
+              <div className="thumbnail-wrapper">
+                <div className="thumbnail-overlay">
+                  <div className="overlay-content">
+                    <div className="project-info">
+                      <h4>The 10 Vinyls</h4>
+                      <p>
+                        <span className="list-title">Tech Stack:</span>&nbsp;
+                        <span className="list">Vue.js, SCSS, TypeScript</span>
+                      </p>
                     </div>
-                  </a>
+                    <a
+                      href="https://meganklusey.github.io/the-10-vinyls/"
+                      className="view-project"
+                      target="_blank"
+                    >
+                      <p>View project</p>
+                      <div className="chevron-forward">
+                        <ion-icon name="chevron-forward-outline"></ion-icon>
+                      </div>
+                    </a>
+                  </div>
                 </div>
+                <img
+                  src={The10Vinyls}
+                  className="thumbnail"
+                  alt="The 10 Vinyls thumbnail"
+                />
               </div>
-              <img
-                src={The10Vinyls}
-                className="thumbnail"
-                alt="The 10 Vinyls thumbnail"
-              />
             </div>
-          </div>
-          <div
-            className="thumbnail-container"
-            data-aos="fade-up"
-            data-aos-anchor-placement="center-bottom"
-            data-aos-delay="100"
-          >
-            <div className="thumbnail-wrapper">
-              <div className="thumbnail-overlay">
-                <div className="overlay-content">
-                  <div className="project-info">
-                    <h4>Hello World / Bye World</h4>
-                    <p>
-                      <span className="list-title">Tech Stack:</span>&nbsp;
-                      <span className="list">GSAP, SVG, SCSS, JavaScript</span>
-                    </p>
-                  </div>
-                  <a
-                    href="https://codepen.io/Megan-K/pen/vEBBKxg"
-                    className="view-project"
-                    target="_blank"
-                  >
-                    <p>View project</p>
-                    <div className="chevron-forward">
-                      <ion-icon name="chevron-forward-outline"></ion-icon>
+            <div
+              className="thumbnail-container"
+              data-aos="fade-up"
+              data-aos-anchor-placement="center-bottom"
+              data-aos-delay="100"
+            >
+              <div className="thumbnail-wrapper">
+                <div className="thumbnail-overlay">
+                  <div className="overlay-content">
+                    <div className="project-info">
+                      <h4>Hello World / Bye World</h4>
+                      <p>
+                        <span className="list-title">Tech Stack:</span>&nbsp;
+                        <span className="list">
+                          GSAP, SVG, SCSS, JavaScript
+                        </span>
+                      </p>
                     </div>
-                  </a>
+                    <a
+                      href="https://codepen.io/Megan-K/pen/vEBBKxg"
+                      className="view-project"
+                      target="_blank"
+                    >
+                      <p>View project</p>
+                      <div className="chevron-forward">
+                        <ion-icon name="chevron-forward-outline"></ion-icon>
+                      </div>
+                    </a>
+                  </div>
                 </div>
+                <img
+                  src={HelloWorldByeWorld}
+                  className="thumbnail cover"
+                  alt="Hello World Bye World thumbnail"
+                />
               </div>
-              <img
-                src={HelloWorldByeWorld}
-                className="thumbnail cover"
-                alt="Hello World Bye World thumbnail"
-              />
             </div>
           </div>
         </div>
-      </div>
-      <div className="work-category">
-        <h3>
-          Professional Projects (collaborative work completed as part of my role
-          at Toru Digital):
-        </h3>
-        <div className="thumbnails-container">
-          <div
-            className="thumbnail-container"
-            data-aos="fade-up"
-            data-aos-anchor-placement="center-bottom"
-          >
-            <div className="thumbnail-wrapper">
-              <div className="thumbnail-overlay">
-                <div className="overlay-content">
-                  <div className="project-info">
-                    <h4>WSM Industries</h4>
-                    <p>
-                      <span className="list-title">Tech Stack:</span>&nbsp;
-                      <span className="list">
-                        WordPress, Tailwind, JavaScript, ScrollMagic
-                      </span>
-                    </p>
-                  </div>
-                  <a
-                    href="https://wsm-ind.com/"
-                    className="view-project"
-                    target="_blank"
-                  >
-                    <p>View project</p>
-                    <div className="chevron-forward">
-                      <ion-icon name="chevron-forward-outline"></ion-icon>
+        <div className="work-category">
+          <h3>
+            Professional Projects (collaborative work completed as part of my
+            role at Toru Digital):
+          </h3>
+          <div className="thumbnails-container">
+            <div
+              className="thumbnail-container"
+              data-aos="fade-up"
+              data-aos-anchor-placement="center-bottom"
+            >
+              <div className="thumbnail-wrapper">
+                <div className="thumbnail-overlay">
+                  <div className="overlay-content">
+                    <div className="project-info">
+                      <h4>WSM Industries</h4>
+                      <p>
+                        <span className="list-title">Tech Stack:</span>&nbsp;
+                        <span className="list">
+                          WordPress, Tailwind, JavaScript, ScrollMagic
+                        </span>
+                      </p>
                     </div>
-                  </a>
+                    <a
+                      href="https://wsm-ind.com/"
+                      className="view-project"
+                      target="_blank"
+                    >
+                      <p>View project</p>
+                      <div className="chevron-forward">
+                        <ion-icon name="chevron-forward-outline"></ion-icon>
+                      </div>
+                    </a>
+                  </div>
                 </div>
+                <img
+                  src={WSMIndustries}
+                  className="thumbnail cover top"
+                  alt="WSM Industries thumbnail"
+                />
               </div>
-              <img
-                src={WSMIndustries}
-                className="thumbnail cover top"
-                alt="WSM Industries thumbnail"
-              />
             </div>
-          </div>
-          <div
-            className="thumbnail-container"
-            data-aos="fade-up"
-            data-aos-anchor-placement="center-bottom"
-            data-aos-delay="50"
-          >
-            <div className="thumbnail-wrapper">
-              <div className="thumbnail-overlay">
-                <div className="overlay-content">
-                  <div className="project-info">
-                    <h4>TSL</h4>
-                    <p>
-                      <span className="list-title">Tech Stack:</span>&nbsp;
-                      <span className="list">WordPress, SCSS, jQuery</span>
-                    </p>
-                  </div>
-                  <a
-                    href="https://tslprojects.com/"
-                    className="view-project"
-                    target="_blank"
-                  >
-                    <p>View project</p>
-                    <div className="chevron-forward">
-                      <ion-icon name="chevron-forward-outline"></ion-icon>
+            <div
+              className="thumbnail-container"
+              data-aos="fade-up"
+              data-aos-anchor-placement="center-bottom"
+              data-aos-delay="50"
+            >
+              <div className="thumbnail-wrapper">
+                <div className="thumbnail-overlay">
+                  <div className="overlay-content">
+                    <div className="project-info">
+                      <h4>TSL</h4>
+                      <p>
+                        <span className="list-title">Tech Stack:</span>&nbsp;
+                        <span className="list">WordPress, SCSS, jQuery</span>
+                      </p>
                     </div>
-                  </a>
+                    <a
+                      href="https://tslprojects.com/"
+                      className="view-project"
+                      target="_blank"
+                    >
+                      <p>View project</p>
+                      <div className="chevron-forward">
+                        <ion-icon name="chevron-forward-outline"></ion-icon>
+                      </div>
+                    </a>
+                  </div>
                 </div>
+                <img
+                  src={TSL}
+                  className="thumbnail cover top"
+                  alt="TSL thumbnail"
+                />
               </div>
-              <img
-                src={TSL}
-                className="thumbnail cover top"
-                alt="TSL thumbnail"
-              />
             </div>
-          </div>
-          <div
-            className="thumbnail-container"
-            data-aos="fade-up"
-            data-aos-anchor-placement="center-bottom"
-            data-aos-delay="100"
-          >
-            <div className="thumbnail-wrapper">
-              <div className="thumbnail-overlay">
-                <div className="overlay-content">
-                  <div className="project-info">
-                    <h4>SEKO</h4>
-                    <p>
-                      <span className="list-title">Tech Stack:</span>&nbsp;
-                      <span className="list">
-                        PHP (Yii2), Tailwind, jQuery, GSAP, ScrollMagic
-                      </span>
-                    </p>
-                  </div>
-                  <a
-                    href="https://www.seko.com/global"
-                    className="view-project"
-                    target="_blank"
-                  >
-                    <p>View project</p>
-                    <div className="chevron-forward">
-                      <ion-icon name="chevron-forward-outline"></ion-icon>
+            <div
+              className="thumbnail-container"
+              data-aos="fade-up"
+              data-aos-anchor-placement="center-bottom"
+              data-aos-delay="100"
+            >
+              <div className="thumbnail-wrapper">
+                <div className="thumbnail-overlay">
+                  <div className="overlay-content">
+                    <div className="project-info">
+                      <h4>SEKO</h4>
+                      <p>
+                        <span className="list-title">Tech Stack:</span>&nbsp;
+                        <span className="list">
+                          PHP (Yii2), Tailwind, jQuery, GSAP, ScrollMagic
+                        </span>
+                      </p>
                     </div>
-                  </a>
+                    <a
+                      href="https://www.seko.com/global"
+                      className="view-project"
+                      target="_blank"
+                    >
+                      <p>View project</p>
+                      <div className="chevron-forward">
+                        <ion-icon name="chevron-forward-outline"></ion-icon>
+                      </div>
+                    </a>
+                  </div>
                 </div>
+                <img
+                  src={SEKO}
+                  className="thumbnail cover top"
+                  alt="SEKO thumbnail"
+                />
               </div>
-              <img
-                src={SEKO}
-                className="thumbnail cover top"
-                alt="SEKO thumbnail"
-              />
             </div>
-          </div>
-          <div
-            className="thumbnail-container"
-            data-aos="fade-up"
-            data-aos-anchor-placement="center-bottom"
-            data-aos-delay="100"
-          >
-            <div className="thumbnail-wrapper">
-              <div className="thumbnail-overlay">
-                <div className="overlay-content">
-                  <div className="project-info">
-                    <h4>Domino Jewellery</h4>
-                    <p>
-                      <span className="list-title">Tech Stack:</span>&nbsp;
-                      <span className="list">PHP (Yii2), Tailwind, jQuery</span>
-                    </p>
-                  </div>
-                  <a
-                    href="https://www.dominojewellery.com/en/"
-                    className="view-project"
-                    target="_blank"
-                  >
-                    <p>View project</p>
-                    <div className="chevron-forward">
-                      <ion-icon name="chevron-forward-outline"></ion-icon>
+            <div
+              className="thumbnail-container"
+              data-aos="fade-up"
+              data-aos-anchor-placement="center-bottom"
+              data-aos-delay="100"
+            >
+              <div className="thumbnail-wrapper">
+                <div className="thumbnail-overlay">
+                  <div className="overlay-content">
+                    <div className="project-info">
+                      <h4>Domino Jewellery</h4>
+                      <p>
+                        <span className="list-title">Tech Stack:</span>&nbsp;
+                        <span className="list">
+                          PHP (Yii2), Tailwind, jQuery
+                        </span>
+                      </p>
                     </div>
-                  </a>
+                    <a
+                      href="https://www.dominojewellery.com/en/"
+                      className="view-project"
+                      target="_blank"
+                    >
+                      <p>View project</p>
+                      <div className="chevron-forward">
+                        <ion-icon name="chevron-forward-outline"></ion-icon>
+                      </div>
+                    </a>
+                  </div>
                 </div>
+                <img
+                  src={DominoJewellery}
+                  className="thumbnail cover top"
+                  alt="Domino Jewellery thumbnail"
+                />
               </div>
-              <img
-                src={DominoJewellery}
-                className="thumbnail cover top"
-                alt="Domino Jewellery thumbnail"
-              />
             </div>
           </div>
         </div>
