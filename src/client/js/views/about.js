0beed05550e1157d1aac090688204ad1ea7befc2
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-02-18 16:08:34 +0000

    Amend about text

--- src/client/js/views/about.js
+++ src/client/js/views/about.js
@@ -6,32 +6,42 @@ const About = () => {
   return (
     <section className="about-container" id="about">
       <div>
-        <p
-          className="aboutText"
-          data-aos-anchor-placement="top-center"
-          data-aos="zoom-in"
-          data-aos-duration="300"
-        >
-          <strong>
-            My passion for web development stems from the opportunity to blend
-            problem-solving with creativity harmoniously.
-          </strong>
+        <p className="aboutText" data-aos-anchor-placement="top-center" data-aos="zoom-in" data-aos-duration="300">
+          <strong>My passion for web development stems from the opportunity to blend problem-solving with creativity harmoniously.</strong>
           <br />
           <br />
-          From sleek aesthetics and seamless functionality, I believe that users
-          worldwide deserve to have a satisfying experience, and this drives me
-          towards innovation and optimisation. I aim to continue growing as a
-          developer and propel myself to new heights.
+          From sleek aesthetics and seamless functionality, I believe that users worldwide deserve to have a satisfying experience, and this drives me towards innovation and optimisation. I aim to
+          continue growing as a developer and propel myself to new heights.
           <br />
           <br />
-          With over four years of commercial experience and a first-class
-          Honours degree in the computing field, my journey so far has equipped
-          me with a diverse skill set, including technologies such as{" "}
-          <strong>React</strong>,&nbsp;
-          <strong>Vue.js</strong>, <strong>TypeScript</strong>,&nbsp;
-          <strong>PHP (Yii2)</strong>, <strong>HTML</strong>,&nbsp;
-          <strong>SCSS</strong>, and <strong>Tailwind</strong>, which are
-          showcased in some of my projects below.
+          With over four years of commercial experience and a first-class Honours degree in the computing field, my journey so far has equipped me with a diverse skill set, including technologies such
+          as&nbsp;
+          <span>
+            <strong>WordPress</strong>,&nbsp;
+          </span>
+          <span>
+            <strong>HTML5</strong>,&nbsp;
+          </span>
+          <span>
+            <strong>SCSS</strong>,&nbsp;
+          </span>
+          <span>
+            <strong>Tailwind</strong>,&nbsp;
+          </span>
+          <span>
+            <strong>JavaScript (ES6+)</strong>,&nbsp;
+          </span>
+          <span>
+            <strong>React</strong>,&nbsp;
+          </span>
+          <span>
+            <strong>Vue.js</strong>,&nbsp;
+          </span>
+          and&nbsp;
+          <span>
+            <strong>PHP (Yii2)</strong>,&nbsp;
+          </span>
+          which are showcased in some of my projects below.
         </p>
       </div>
     </section>
