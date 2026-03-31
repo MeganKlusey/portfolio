fa7165536c261cbe14fac5064b3fac4f5cbf88e0
MeganKlusey <megan.klusey@hotmail.co.uk>
2026-03-31 01:58:00 +0100

    About text update

--- src/client/js/views/about.js
+++ src/client/js/views/about.js
@@ -7,41 +7,34 @@ const About = () => {
     <section className="about-container" id="about">
       <div>
         <p className="aboutText" data-aos-anchor-placement="top-center" data-aos="zoom-in" data-aos-duration="300">
-          <strong>My passion for web development stems from the opportunity to blend problem-solving with creativity harmoniously.</strong>
+          <strong>I'm a front-end developer with 4+ years of commercial experience building clean, user-focused websites and web applications within a digital agency environment.</strong>
           <br />
           <br />
-          From sleek aesthetics and seamless functionality, I believe that users worldwide deserve to have a satisfying experience, and this drives me towards innovation and optimisation. I aim to
-          continue growing as a developer and propel myself to new heights.
+          I’ve worked closely with designers, developers, and project managers to deliver high-quality websites for a range of clients. Alongside development, I regularly test usability and identify
+          improvements, such as refining responsive layouts to ensure a consistent experience across devices.
           <br />
           <br />
-          With over four years of commercial experience and a first-class Honours degree in the computing field, my journey so far has equipped me with a diverse skill set, including technologies such
-          as&nbsp;
+          My experience includes technologies such as&nbsp;
           <span>
             <strong>WordPress</strong>,&nbsp;
           </span>
           <span>
-            <strong>HTML5</strong>,&nbsp;
+            <strong>HTML</strong>,&nbsp;
           </span>
           <span>
             <strong>SCSS</strong>,&nbsp;
           </span>
           <span>
-            <strong>Tailwind</strong>,&nbsp;
-          </span>
-          <span>
-            <strong>JavaScript (ES6+)</strong>,&nbsp;
+            <strong>JavaScript</strong>,&nbsp;
           </span>
           <span>
             <strong>React</strong>,&nbsp;
           </span>
+          and&nbsp;
           <span>
             <strong>Vue.js</strong>,&nbsp;
           </span>
-          and&nbsp;
-          <span>
-            <strong>PHP (Yii2)</strong>,&nbsp;
-          </span>
-          which are showcased in some of my projects below.
+          which are demonstrated in the projects below.
         </p>
       </div>
     </section>
